e0100639c5bcb477c3dc6f0886679ccb6490214b
Francis Kapakasa <franciskapakasa@gmail.com>
2023-03-21 19:14:59 +0000

    for loop fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,26 +2,24 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
- *
- *
- * Return: Always 0.
  */
 
 void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (n, n > 98, n--)
+		for (n; n > 98; n--)
 		{
-			printf(n", ");
+			printf("%d\n", n);
+
 		}
 		printf(98"\n");
 	}
 	else
 	{
-		for (n, n <= 98, n++)
+		for (n; n <= 98; n++)
 		{
-			printf(n", ");
+			printf("%d\n", n);
 		}
 		printf(98"\n");
 	}
