6c2309f5ccbacf66af02d156645d11d821b5cbb9
Francis Kapakasa <franciskapakasa@gmail.com>
2023-03-21 16:24:34 +0000

    change of header file to inclued quotation marks

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _abs - calculates the absolute value of an integer
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * jack_bauer - prints the every minute up to 24 hours
