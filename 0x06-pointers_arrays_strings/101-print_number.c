4a8acbcb4c3a9cc9ad78147d1a94907d427db94a
Francis Kapakasa <franciskapakasa@gmail.com>
2023-03-30 09:30:55 +0000

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,24 +7,9 @@
 
 void print_number(int n)
 {
-	unsigned int _n;
-	/* char num = "0123456789"; */
-
 	if (n < 0)
 	{
-		_n = -n;
 		_putchar('-');
 	}
-
-	else
-	{
-		_n = n;
-	}
-
-	if (_n >= 10)
-	{
-		print_number(_n / 10);
-	}
-
-	_putchar('0' + (_n % 10));
+	_putchar('0' + (n);
 }
