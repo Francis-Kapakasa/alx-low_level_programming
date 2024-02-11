3e643b2adbb62ed66fdce9f1ba6f7414498362e4
Francis-Kapakasa <franciskapakasa@gmail.com>
2024-02-11 11:48:30 +0000

    changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * _puts_recursion - Prints string
+ * @s - a pointer to a character in sting
+ *
+ * Return: 1 on success
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return 1;
+	}
+	_putchar (s);
+	*s = *s + 1;
+	_puts_recursion(*s);
+}
