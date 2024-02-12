20603e569914ef06c786f4e7ba1f2fb3897a72f9
Francis-Kapakasa <franciskapakasa@gmail.com>
2024-02-12 16:13:50 +0000

    updated puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,15 @@
 
 void _puts_recursion(char *s)
 {
-	if (!*s)
+	if (*s)
 	{
-		_putchar('\n');
-		
+		_putchar (*s);
+		s = s + 1;
+		_puts_recursion (s);
 	}
-	_putchar (*s);
-	s = s + 1;
-	_puts_recursion (s);
+
+	else
+	{
+		_putchar ('\n');
+	}
 }
