b64805684a020fd9df94d254e5c7fe7bf3c6416f
Francis-Kapakasa <franciskapakasa@gmail.com>
2024-02-12 17:00:25 +0000

    new rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ *
+ * @s: characters in a string
+ *
+ * Return: on success
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if(*s)
+	{
+		s = s + 1;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
